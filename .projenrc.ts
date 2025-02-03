ef69b402eded3c68f4f4086672653dc02ccc0988
Mathias von Kaiz <127110360+mvk-abs@users.noreply.github.com>
2025-02-03 14:14:29 +0000

    chore(projen): removed readme

--- .projenrc.ts
+++ .projenrc.ts
@@ -18,66 +18,12 @@ export const project = new JsiiProject({
   jestEnabled: true,
   prettierEnabled: true,
   vscodeEnabled: true,
-  readme: {
-    contents: `x`,
-  },
+
   // Use projen standard API for jest.
   // For the other modules like vscode, devContainer, etc.
   // we use own configuration which are set to false as defaults
   // in the JsiiProject itself
   jest: true,
 });
-
-// export const project = new TerraformModuleProject({
-//   name: '@dxfrontier/projen-template-projects',
-//   // repositoryUrl: 'https://github.com/dxfrontier/projen-template-projects.git',
-//   // author: 'DXFrontier Dev Team',
-//   // authorAddress: 'mathias.von-kaiz@abs-gmbh.de',
-//   copyrightOwner: 'ABS GmbH',
-//   defaultReleaseBranch: 'dev',
-
-//   commitLintEnabled: true,
-//   devContainerEnabled: true,
-//   eslintEnabled: true,
-//   githubEnabled: true,
-//   huskyEnabled: true,
-//   jestEnabled: true,
-//   prettierEnabled: true,
-//   typescriptEnabled: true,
-//   vscodeEnabled: true,
-//   sampleCodeEnabled: true,
-
-//   // use projen standard jest and npm
-//   // the others like vscode, devContainer, ...
-//   // we use own configuration which are set to false as defaults
-//   // in the JsiiProject itself
-//   jest: true,
-// });
-
-// export const project = new GitHubActionProject({
-//   name: '@dxfrontier/projen-template-projects',
-//   // repositoryUrl: 'https://github.com/dxfrontier/projen-template-projects.git',
-//   // author: 'DXFrontier Dev Team',
-//   // authorAddress: 'mathias.von-kaiz@abs-gmbh.de',
-//   copyrightOwner: 'ABS GmbH',
-//   defaultReleaseBranch: 'dev',
-
-//   commitLintEnabled: true,
-//   devContainerEnabled: true,
-//   eslintEnabled: true,
-//   githubEnabled: true,
-//   huskyEnabled: true,
-//   jestEnabled: true,
-//   prettierEnabled: true,
-//   typescriptEnabled: true,
-//   vscodeEnabled: true,
-//   sampleCodeEnabled: true,
-
-//   // use projen standard jest and npm
-//   // the others like vscode, devContainer, ...
-//   // we use own configuration which are set to false as defaults
-//   // in the JsiiProject itself
-//   jest: true,
-// });
 
 project.synth();
