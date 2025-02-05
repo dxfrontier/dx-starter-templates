8ae5b08a4e9614b7f72656f2abec999d40426c5f
dragolea <dragolea@yahoo.com>
2025-02-05 09:42:15 +0200

    test(github-action): completed the github-test

--- test/github-action/project.test.ts
+++ test/github-action/project.test.ts
@@ -42,6 +42,71 @@ jest.mock('projen/lib/typescript', (): any => {
   };
 });
 
+const expectedReadmeTemplateLines: string[] = [
+  `# my-github-action`,
+  '',
+  `Github project`,
+  '',
+  '# Table of Contents',
+  '',
+  `- [my-github-action](#my-github-action)`,
+  '- [Table of Contents](#table-of-contents)',
+  '  - [`Setup`](#setup)',
+  '  - [Project tools and utilities](#project-tools-and-utilities)',
+  '    - [Prettier](#prettier)',
+  '    - [Lint-staged](#lint-staged)',
+  '    - [Commit-Lint](#commit-lint)',
+  '    - [Husky](#husky)',
+  '  - [Authors](#authors)',
+  '',
+  '## `Setup`',
+  '',
+  '1. Create the following three labels in your repository by navigating to `Pull Requests` > `Labels` > `New Label`:',
+  '',
+  '   - **patch**',
+  '   - **minor**',
+  '   - **major**',
+  '',
+  '> [!TIP]',
+  '> For more info about `SemVer (MAJOR.MINOR.PATCH)`, visit [SemVer.org](https://semver.org/)',
+  '',
+  '## Project tools and utilities',
+  '',
+  'The following tools/utilities are predefined when you create the project using `projen` and do not require any configuration.',
+  '',
+  '### Prettier',
+  '',
+  'A code formatter that enforces consistent style across your project automatically.',
+  '',
+  '> [!NOTE]',
+  '> Prettier is triggered when a new commit is committed.',
+  '',
+  '> [!TIP]',
+  '> You can also run it manually by executing:',
+  '> ```bash',
+  '> npm run prettier:fix',
+  '> ```',
+  '',
+  '### Lint-staged',
+  '',
+  'Runs linters (e.g., Prettier, ESLint) only on staged Git files before committing.',
+  '',
+  '### Commit-Lint',
+  '',
+  'Ensures commit messages follow a specified format (e.g., Conventional Commits).',
+  '',
+  '### Husky',
+  '',
+  'Git hooks manager to automate pre-commit and pre-push tasks (e.g., running tests or linters).',
+  '',
+  '## Authors',
+  '',
+  '- [@Author1](#link1)',
+  '- [@Author2](#link2)',
+  '',
+  '<p align="right">(<a href="#table-of-contents">back to top</a>)</p>',
+];
+
 describe('GitHubActionsProject Constructor Options', (): void => {
   afterEach((): void => {
     jest.resetAllMocks();
@@ -58,6 +123,7 @@ describe('GitHubActionsProject Constructor Options', (): void => {
   test('Configuration is set properly', async (): Promise<void> => {
     const options: GitHubActionProjectOptions = {
       name: 'my-github-action',
+      description: 'Github project',
       defaultReleaseBranch: 'dev', // this is needed due to https://github.com/projen/projen/pull/524
       commitlintEnabled: true,
       devContainerEnabled: true,
@@ -106,6 +172,11 @@ describe('GitHubActionsProject Constructor Options', (): void => {
       release: false,
       pullRequestTemplate: false,
       depsUpgrade: false,
+
+      readme: {
+        filename: 'README.md',
+        contents: expectedReadmeTemplateLines.join('\n'),
+      },
     });
   });
 });
