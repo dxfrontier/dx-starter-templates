bcad6a91b33177f5733c0007094d78b1e030a663
dragolea <dragolea@yahoo.com>
2025-03-14 12:25:35 +0200

    chore(lib): regenerated lib

--- lib/cap-service/config/github.d.ts
+++ lib/cap-service/config/github.d.ts
@@ -4,21 +4,9 @@ import { GitHubConfigBase } from '../../base';
  */
 export declare class GitHubConfigCapService extends GitHubConfigBase {
     protected get configFilePullRequest(): Record<string, string[]>;
-    protected get configFileFeatureIssue(): Record<string, string[]>;
-    /**
-     * Retrieves the configuration for the story issue template file.
-     *
-     * @returns A record where the key is the file path and the value is an array of strings
-     *          representing the content of the issue template.
-     */
-    protected get configFileStoryIssue(): Record<string, string[]>;
     protected get configFileReleaseWorkflow(): Record<string, string[]>;
     protected get configFileDeploymentWorkflow(): Record<string, string[]>;
     protected get configs(): Record<string, string[]>[];
-    /**
-     * Creates a user story issue template file.
-     */
-    createStoryIssue(): void;
     /**
      * Creates a deployment workflow template file.
      */
